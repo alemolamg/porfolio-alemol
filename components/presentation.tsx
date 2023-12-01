106fe85169d308c2eceb4b8481ce1a8751e60c45
Alemol <alemolamg@outlook.com>
2023-12-01 21:00:51 +0100

    work in component

---
+++ components/presentation.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./presentation.module.scss";
+
+interface ImageWithTitleProps {}
+
+const Presentation: React.FC = () => {
+  const imageURL = "/public/imagenAlemol.jpeg";
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Alejandro Molero Gómez</h1>
+      <Image
+        className={styles.image}
+        src={"/imagenAlemol.jpeg"}
+        alt="Error imagen"
+        width={150}
+        height={100}
+        priority
+      />
+    </div>
+  );
+};
+
+export default Presentation;
