60f8ef556c30923df31c0a9d9857ae55c7086414
Alemol <alemolamg@outlook.com>
2023-12-03 19:48:14 +0100

    work on first view component

--- components/presentation.tsx
+++ components/presentation.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 import styles from "./presentation.module.scss";
 
 const Presentation: React.FC = () => {
-  const imageURL = "/public/imagenAlemol.jpeg";
+  const imageURL = "/imagenAlemol.jpeg";
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Alejandro Molero Gómez</h1>
       <Image
         className={styles.image}
-        src={"/imagenAlemol.jpeg"}
-        alt="Error imagen"
-        width={150}
-        height={100}
+        src={imageURL}
+        alt="imagen de Alejandro Molero"
+        width={200}
+        height={200}
         priority
       />
     </div>
