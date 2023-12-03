8c58d7ef5800f707a0280dc381accdb6dea8a0b9
Alemol <alemolamg@outlook.com>
2023-12-03 20:14:33 +0100

    Presentation component version 1.0

--- components/presentation.tsx
+++ components/presentation.tsx
@@ -5,16 +5,19 @@ import styles from "./presentation.module.scss";
 const Presentation: React.FC = () => {
   const imageURL = "/imagenAlemol.jpeg";
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Alejandro Molero Gómez</h1>
-      <Image
-        className={styles.image}
-        src={imageURL}
-        alt="imagen de Alejandro Molero"
-        width={200}
-        height={200}
-        priority
-      />
+    <div className={styles.container} >
+      <div className={styles.head}>
+        <h1 className={styles.title}>Alejandro Molero Gómez</h1>
+        <Image
+          className={styles.image}
+          src={imageURL}
+          alt="imagen de Alejandro Molero"
+          width={200}
+          height={200}
+          priority
+        />
+      </div>
+      <p className="flex justify-center text-xl bg-blue-800">texto descriptivo</p>
     </div>
   );
 };
