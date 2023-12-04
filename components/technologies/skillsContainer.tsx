da81b690555a560c1268a608340833763d6aeee0
Alemol <alemolamg@outlook.com>
2023-12-04 14:36:11 +0100

    work on skillContainer

--- components/technologies/skillsContainer.tsx
+++ components/technologies/skillsContainer.tsx
@@ -1,4 +1,5 @@
 import SkillCard from "./skillCard";
+import styles from "./skill.module.scss";
 
 interface SkillsProps {}
 
@@ -10,13 +11,13 @@ export interface Skill {
 const skills: Skill[] = [
   { name: "python", icon: "/skill-icons/python_icon.svg" },
   { name: "kotlin", icon: "/skill-icons/Kotlin-Full-Color.svg" },
-  // { name: "sql", icon: "image" },
+  { name: "sql", icon: "/skill-icons/kotlin_icon.svg" },
   // { name: "java", icon: "image" },
 ];
 
 const SkillsContainer: React.FC<SkillsProps> = ({}) => {
   return (
-    <div className="flex flex-wrap gap-5 justify-around mt-5">
+    <div className={styles.skillContainer}>
       {skills.map((tecnologia, index) => (
         <SkillCard key={index} skill={tecnologia} />
       ))}
