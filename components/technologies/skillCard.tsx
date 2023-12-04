e1655c40bff71d63eadc48e4ffe0c488d6130600
Alemol <alemolamg@outlook.com>
2023-12-04 11:58:41 +0100

    add svg images

--- components/technologies/skillCard.tsx
+++ components/technologies/skillCard.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import styles from "./skillCard.module.scss";
+import { Skill } from "@/app/page";
+import Image from "next/image";
 
 interface SkillProps {
-  name: string;
+  skill: Skill;
 }
 
-const SkillCard: React.FC<SkillProps> = ({ name }) => {
+const SkillCard: React.FC<SkillProps> = ({ skill }) => {
   return (
     <div className={styles.container}>
-      <h2>{name}</h2>
-      <p>imagen</p>
+      <h2>{skill.name}</h2>
+      <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src={skill.icon}
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      {/* <img src={skill.icon} alt={skill.name} /> */}
     </div>
   );
 };
