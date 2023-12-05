4184385aa0548164face05ae313c4296b09991e0
Alemol <alemolamg@outlook.com>
2023-12-05 19:25:41 +0100

    first footer edition

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,13 +1,52 @@
 import React from "react";
 import styles from "./footer.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  IconDefinition,
+  faApple,
+  faApplePay,
+  faCcApplePay,
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
-const Footer: React.FC = () => {
-
-    return (
-        <footer className={styles.footer}>
-            <h4>@Alemol AMG</h4>
-        </footer>
-    )
+export interface LinkIcons {
+  enlace: string;
+  icon: IconDefinition;
 }
 
-export default Footer
+const linkList: LinkIcons[] = [
+  { enlace: "https://www.linkedin.com/in/alemolamg/", icon: faLinkedin },
+  { enlace: "https://github.com/alemolamg", icon: faGithub },
+  { enlace: "https://x.com/alemolamg", icon: faXTwitter },
+];
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      {/* <div>Contenido de Alemol</div> */}
+      <div>
+        <h4>@Alemol AMG</h4>
+      </div>
+      <div>
+        enlaces:
+        <div className={styles.links}>
+          {linkList.map((link, index) => (
+            <a
+              key={index}
+              href={link.enlace}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon className={styles.icon} icon={link.icon} />
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
