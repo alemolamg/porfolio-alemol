77ce4fb4fb9bae7ded402eada17c2243096818a1
Alemol <alemolamg@outlook.com>
2023-12-05 10:41:30 +0100

    update image for presentation

--- components/presentation.tsx
+++ components/presentation.tsx
@@ -1,21 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Image from "next/image";
 import styles from "./presentation.module.scss";
 
 const Presentation: React.FC = () => {
+  const linkedin_image =
+    "https://media.licdn.com/dms/image/C4E03AQFyWz4Hfu6YWQ/profile-displayphoto-shrink_800_800/0/1656794215383?e=1707350400&v=beta&t=OF2GgRv26NwArHiAq-8VoarPhKxSBTcT7ug62HBcHEY";
   const imageURL = "/imagenAlemol.jpeg";
   return (
     <div className={styles.container}>
       <div className={styles.head}>
         <h1 className={styles.title}>Alejandro Molero Gómez</h1>
-        <Image
+        {/* <Image
           className={styles.image}
           src={imageURL}
           alt="imagen de Alejandro Molero"
           width={200}
           height={200}
           priority
+        /> */}
+        {/* <div className={styles.imageContainer}> */}
+        <img
+          className={styles.image}
+          src={linkedin_image}
+          alt="Descripción de la imagen"
         />
+        {/* </div> */}
       </div>
       <div className="justify-center bg-blue-800">
         <h3 className="mx-5 md:mx-16 text-2xl">Descripción profesional </h3>
