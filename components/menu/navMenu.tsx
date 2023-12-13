f2468a59b3b769d63cdeafd8476f70e6b4d5d13d
Alemol <alemolamg@outlook.com>
2023-12-13 19:43:49 +0100

    Add X icon

--- components/menu/navMenu.tsx
+++ components/menu/navMenu.tsx
@@ -4,21 +4,35 @@ import React, { FC, useState } from "react";
 import styles from "./menu.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import cx from "clsx";
+import "animate.css";
 
 const MyComponent: FC = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
 
   const handleClick = () => {
     setOpen(!open);
   };
 
+  const iconbutton = !open ? (
+    <FontAwesomeIcon className={styles.icon} icon={faBars} />
+  ) : (
+    <FontAwesomeIcon className={styles.icon} icon={faXmark} />
+  );
+
   return (
     <div className={styles.navbar}>
       <div className={styles.menuIcon} onClick={handleClick}>
-        <FontAwesomeIcon className={styles.icon} icon={faBars} />
+        {iconbutton}
       </div>
       {open && (
-        <div className={styles.menuPanel}>
+        <div
+          className={cx(
+            styles.menuPanel,
+            "animate__animated animate__fadeInRight"
+          )}
+        >
           <ul>
             <li>Inicio</li>
             <li>Acerca de</li>
