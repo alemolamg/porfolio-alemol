58317c6985d16c256d01b37f1ff63305516682b3
Alemol <alemolamg@outlook.com>
2023-12-13 13:32:07 +0100

    reconfigured

--- components/presentation.tsx
+++ components/presentation.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-import Image from "next/image";
 import styles from "./presentation.module.scss";
 
 const Presentation: React.FC = () => {
@@ -11,21 +10,11 @@ const Presentation: React.FC = () => {
     <div className={styles.container}>
       <div className={styles.head}>
         <h1 className={styles.title}>Alejandro Molero Gómez</h1>
-        {/* <Image
-          className={styles.image}
-          src={imageURL}
-          alt="imagen de Alejandro Molero"
-          width={200}
-          height={200}
-          priority
-        /> */}
-        {/* <div className={styles.imageContainer}> */}
         <img
           className={styles.image}
           src={linkedin_image}
           alt="Descripción de la imagen"
         />
-        {/* </div> */}
       </div>
       <div className={styles.description}>
         <h3 className="mx-5 md:mx-20 text-2xl">Descripción profesional </h3>
