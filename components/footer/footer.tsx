770e5ba06171655827a9f95e47d4851693b9971a
Alemol <alemolamg@outlook.com>
2023-12-25 13:05:02 +0100

    update footer with email

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,11 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LinkIcons from "../utils/links";
 
 const Footer: React.FC = () => {
+  const myEmail = "hello@alemolamg.com"
   return (
     <footer className={styles.footer}>
       {/* <div>Contenido de Alemol</div> */}
       <div>
-        <h4>@Alemol AMG</h4>
+        {/* <h4>@Alemol AMG</h4> */}
+        <p><a href={`mailto:${myEmail}`}>{myEmail}</a></p>
       </div>
       <div>
         enlaces:
